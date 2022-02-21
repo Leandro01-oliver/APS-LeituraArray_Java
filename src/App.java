5f5dcdd068f7161aeffb9c62cd4ab82157def5db
Leandro01-oliver <leandro.oliver_lima@hotmail.com>
2022-02-21 16:44:20 -0400

    Envio

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ReadingArray readingArray = new ReadingArray();
+        readingArray.CreateFileArray();
+    }
+}
